a4284744b2d2aa38312381658da96c0dffcf0f5c
spirit-92 <panasenkodima97@gmail.com>
2020-06-29 04:52:37 +0300

    brain

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept");
-  next();
-})
+
 app.use(express.static(path.join(__dirname, 'dist/brainstorming')));
 
 app.get('*', (req, res) => {
