6a5cbeb8215894c62bb0bbb17b4d4fe5ce2392ee
Dima <spirit.92@inbox.ru>
2021-10-11 17:07:04 +0300

    q

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {ProductService} from "../services/product.service";
 import {Product, ProductB2b} from "../shared/interfaces";
 import {ActivatedRoute} from "@angular/router";
@@ -9,14 +9,17 @@ import {ActivatedRoute} from "@angular/router";
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss']
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent implements OnInit  {
   product: any
   black:boolean = true
   white:boolean;
   productOption:ProductB2b
+  sum = 0
   constructor(
     private serviceProduct: ProductService,
     private route: ActivatedRoute,
+    private elRef:ElementRef,
+
   ) {
   }
 
@@ -35,6 +38,7 @@ export class ProductPageComponent implements OnInit {
       console.log(this.productOption)
     })
   }
+
   changeColor(color){
     if (color === 'black'){
       this.white = false
@@ -43,6 +47,19 @@ export class ProductPageComponent implements OnInit {
       this.white = true
       this.black = false
     }
+
+  }
+
+  changeCount(event:Event,price,idTotal) {
+    let allPrice = this.elRef.nativeElement.querySelectorAll(`.priceClass`)
+    let sum = 0
+    allPrice.forEach( price =>{
+      console.log(price.value , price.getAttribute('data-price'))
+
+      let totalPrice = price.value * price.getAttribute('data-price')
+      sum += +totalPrice
+    })
+    this.sum = sum
 
   }
 
